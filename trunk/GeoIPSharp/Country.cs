608d32d4026289c9dff58f042674e6fac29e4191
greglio.GB <greglio.GB@8620aec0-2ceb-11df-8b4d-31f007950f82>
2010-04-14 12:19:28 +0000

    Fixed some StyleCop issues

--- trunk/GeoIPSharp/Country.cs
+++ trunk/GeoIPSharp/Country.cs
@@ -22,8 +22,22 @@ namespace MaxMind.GeoIP
 {
     using System;
 
+    /// <summary>
+    /// Represents a country and it's ISO code.
+    /// </summary>
     public class Country : IEquatable<Country>
     {
+        /// <summary>
+        /// Initializes a new instance of the MaxMind.GeoIP.Country class.
+        /// </summary>
+        /// <param name="code">The country code.</param>
+        /// <param name="name">The country name.</param>
+        public Country(string code, string name)
+        {
+            this.Code = code;
+            this.Name = name;
+        }
+
         /// <summary>
         /// Gets the ISO two-letter country code of this country.
         /// </summary>
@@ -34,43 +48,6 @@ namespace MaxMind.GeoIP
         /// </summary>
         public string Name { get; private set; }
 
-        /// <summary>
-        /// Creates a new Country.
-        /// </summary>
-        /// <param name="code">The country code.</param>
-        /// <param name="name">The country name.</param>
-        public Country(string code, string name)
-        {
-            Code = code;
-            Name = name;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0} ({1})", Name, Code);
-        }
-
-        #region IEquatable<Country> Members
-
-        public bool Equals(Country other)
-        {
-            return this.Code == other.Code && this.Name == other.Name;
-        }
-
-        #endregion
-
-        public override bool Equals(object obj)
-        {
-            Country other = obj as Country;
-            if (other == null) return false;
-            return Equals(other);
-        }
-
-        public override int GetHashCode()
-        {
-            return this.Code.GetHashCode() ^ this.Name.GetHashCode();
-        }
-
         public static bool operator ==(Country a, Country b)
         {
             return a.Equals(b);
@@ -79,6 +56,56 @@ namespace MaxMind.GeoIP
         public static bool operator !=(Country a, Country b)
         {
             return !a.Equals(b);
+        }
+
+        /// <summary>
+        /// Formats this Country object as a human readable string.
+        /// </summary>
+        /// <returns>Returns a string that represents the current MaxMind.GeoIP.Country.</returns>
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", this.Name, this.Code);
+        }
+
+        #region IEquatable<Country> Members
+
+        /// <summary>
+        /// Determines whether the specified MaxMind.GeoIP.Country is equal to the current MaxMind.GeoIP.Country.
+        /// </summary>
+        /// <param name="other">The MaxMind.GeoIP.Country to compare with the current MaxMind.GeoIP.Country.</param>
+        /// <returns>Returns true if the specified MaxMind.GeoIP.Country is equal to the current MaxMind.GeoIP.Country;
+        /// otherwise, false.</returns>
+        public bool Equals(Country other)
+        {
+            return this.Code == other.Code && this.Name == other.Name;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Determines whether the specified System.object is equal to the current MaxMind.GeoIP.Country.
+        /// </summary>
+        /// <param name="obj">The System.object to compare with the current MaxMind.GeoIP.Country.</param>
+        /// <returns>Returns true if the specified System.object is equal to the current MaxMind.GeoIP.Country;
+        /// otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            Country other = obj as Country;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Equals(other);
+        }
+
+        /// <summary>
+        /// Returns the hash code for this MaxMind.GeoIP.Country.
+        /// </summary>
+        /// <returns>Returns a 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            return this.Code.GetHashCode() ^ this.Name.GetHashCode();
         }
     }
 }
